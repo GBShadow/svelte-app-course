d729f51acdadac08bd51ce08ac2c391b198ec52d
GBShadow <gbs.shadow@gmail.com>
2023-10-13 20:51:57 -0300

    routes and list of posts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,19 @@
-<h1>Home Page</h1>
+<script lang="ts">
+	import type { PageData } from './$types'
+
+	export let data: PageData
+
+	$: ({ posts } = data)
+</script>
+
+<h1>Posts</h1>
+
+<h3>Showing {posts.length} posts.</h3>
+
+{#each posts as { slug, title }, i}
+	<ul>
+		<li>
+			<a href={`/posts/${slug}`}>{title}</a>
+		</li>
+	</ul>
+{/each}
